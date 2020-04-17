ea3f17bc51b6ec734226cdaf2e46fe3c1d4131e6
Chogrammer26 <48506056+Chogrammer26@users.noreply.github.com>
2020-04-17 13:30:04 +0800

    Create mp_ex2.cpp

---
+++ mp_ex2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+void input(int [][], int, int);
+void result(int [][], int, int);
+
+int main()
+{
+    int row,col;
+    std::cin >> row >> col;
+    
+    int arr[row][col];
+    input(arr,row,col);
+    result(arr,row,col;
+}
+
+void input(int arr[][], int row, int col)
+{
+    for (int i = 0; i < row, i++)
+    for (int j = 0; j < col, j++)
+    std::cin >> arr[i][j]; 
+}
+
+void result(int arr[][], int row, int col)
+{    
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) 
+            std::cout << *(*(arr + i) + j) << " ";
+    std::cout << '\n';
+    }
+
+}
